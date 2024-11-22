59dbc78f69e2f94007ac966c98c54a4e88822474
hashLahiru <hasinthalahiru333@gmail.com>
2024-11-22 11:15:16 +0530

    Header-Video-Order Completed

--- resources/views/components/header/header-video-order.blade.php
+++ resources/views/components/header/header-video-order.blade.php
@@ -1,20 +1,20 @@
 <div class="header-1">
-  <div class="page-header" style="background-image: url('{{ $headerData->background_image_url }}');">
+  <div class="page-header" style="background-image: url('{{ $headerData->background_image_url ?? null }}');">
     <div class="filter"></div>
     <div class="content-center">
       <div class="container">
         <div class="row">
           <div class="col-md-5">
             <div class="iframe-container">
-              <iframe src="{{ $headerData->video_url }}" frameborder="0" allowfullscreen height="250"></iframe>
+              <iframe src="{{ $headerData->video_url ?? null }}" frameborder="0" allowfullscreen height="250"></iframe>
             </div>
           </div>
           <div class="col-md-6 ml-auto">
-            <h2 class="title">{{ $headerData->title }}</h2>
-            <h5 class="description">{{ $headerData->description }}</h5>
+            <h2 class="title">{{ $headerData->title ?? null }}</h2>
+            <h5 class="description">{{ $headerData->description ?? null }}</h5>
             <br />
-            <a href="{{ $headerData->button_url }}" target="_blank" class="btn btn-danger">
-              <i class="{{ $headerData->button_icon }}"></i> {{ $headerData->button_text }}
+            <a href="{{ $headerData->button_url ?? null }}" target="_blank" class="btn btn-danger">
+              <i class="{{ $headerData->button_icon ?? null }}"></i> {{ $headerData->button_text ?? null }}
             </a>
           </div>
         </div>
