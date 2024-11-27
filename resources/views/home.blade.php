3b21e2c0f712d61c9a7c7502afcc8f71775d5dc5
hashLahiru <hasinthalahiru333@gmail.com>
2024-11-27 13:18:59 +0530

    layout fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,16 +8,8 @@
 
 @include('components.product', ['productData' => $productData])
 
-@include('components.features.features-2', ['feature2Data' => $feature2Data])
-@include('components.features.features-3', ['feature3Data' => $feature3Data])
-@include('components.features.features-4', ['feature4Data' => $feature4Data])
-@include('components.features.features-5', ['feature5Data' => $feature5Data])
-
-
 <div class="wrapper">
-
     @component('components.about-us') @endcomponent
     @component('components.cta') @endcomponent
-
 </div>
 @endsection
