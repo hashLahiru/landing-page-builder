9c26a56344d017de251e3dc03f6f4565dafdea9d
hashLahiru <hasinthalahiru333@gmail.com>
2024-11-28 14:29:23 +0530

    aaa

---
+++ resources/views/components/layout/navbar.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg navbar-transparent fixed-top">
+    <div class="container">
+        <div class="navbar-translate">
+            <a class="navbar-brand" href="/" rel="tooltip" title="Paper Kit 2 PRO" data-placement="bottom">
+                Paper Kit 2 Pro
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
+                aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="navigation" data-nav-image="{{ asset('img/blurred-image-1.jpg') }}">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="/" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#about" class="nav-link">About Us</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown">
+                        Components
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
